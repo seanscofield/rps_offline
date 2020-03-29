171320bbf1f35da07a0cf20d75c718c78bfc9a4e
Lucas Lofaro <lucaslofaro@gmail.com>
2020-03-29 01:05:51 -0700

    refactor: Factor out game script
    Moves the main game script into a separate file for easier
    development workflow.

---
+++ index.js
@@ -0,0 +1,62 @@
+var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: { y: 0 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('circle', 'assets/white_circle.png');
+}
+
+function create ()
+{
+    // create a collision group for player circle
+    var playerBalls = this.physics.add.group({
+        bounceX: 1,
+        bounceY: 1
+    });
+
+    // create a collision group for enemy circles
+    var enemies = this.physics.add.group({
+        bounceX: 1,
+        bounceY: 1
+    });
+
+    // create player circle
+    var player = this.physics.add.image(300, 240, 'circle'); // create player as circle on screen
+    playerBalls.add(player) // add player to group
+    player.body.setCircle(45); // this sets the radius of the player's collider size
+    player.body.collideWorldBounds = true; // set to True so that player can collide with edge of screen
+    this.player = player // this is needed for the update function below
+
+    // create enemy circle (same sort of logic as above)
+    var enemy = this.physics.add.image(600, 240, 'circle');
+    enemies.add(enemy)
+    enemy.body.setCircle(45);
+    enemy.body.collideWorldBounds = true;
+
+    // make it so that items in the playerBalls group can collide with items in the enemies group
+    this.physics.add.collider(playerBalls, enemies);
+}
+
+function update()
+{
+    // get mouse coordinates, and move player towards mouse
+    var pointer = this.input.activePointer;
+    this.physics.moveToObject(this.player, pointer, 240);
+}
