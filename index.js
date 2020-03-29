15db1e8cb3dea87047fcb345b0bf9670e3e07aa8
Lucas Lofaro <lucaslofaro@gmail.com>
2020-03-29 04:53:02 -0700

    Add new ball images

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('circle', 'assets/white_circle.png');
-    this.load.image('circle_small', 'assets/white_circle_now_working.png');
+    this.load.image('air', 'assets/SVG/sign-air.svg');
+    this.load.image('earth', 'assets/SVG/sign-earth.svg');
+    this.load.image('fire', 'assets/SVG/sign-fire.svg');
+    this.load.image('water', 'assets/SVG/sign-water.svg');
 }
 
 function create ()
@@ -39,32 +41,37 @@ function create ()
     });
 
     // create player circle
-    var player = this.physics.add.image(300, 240, 'circle');  // create player as circle on screen
-    playerBalls.add(player)  // add player to group
+    var player = this.physics.add.image(300, 240, 'air'); // create player as circle on screen
+    playerBalls.add(player) // add player to group
     add_circular_collider(player)
     
-    this.player = player  // this is needed for the update function below
+    this.player = player // this is needed for the update function below
 
-    // create enemy circle 
-    var enemy = this.physics.add.image(600, 240, 'circle_small');
-    enemies.add(enemy)
-    add_circular_collider(enemy)
+    for (element of ['earth', 'fire', 'water']) {
+        // create enemy circle
+        x_offset = Math.floor(Math.random()*500)
+        y_offset = Math.floor(Math.random()*500)
+        var enemy = this.physics.add.image(600 + x_offset, 250 + y_offset, element);
+        enemies.add(enemy)
+        add_circular_collider(enemy)
+    }
 
     // make it so that items in the playerBalls group can collide with items in the enemies group
     this.physics.add.collider(playerBalls, enemies);
+    this.physics.add.collider(enemies, enemies);
 }
 
 function update()
 {
     // get mouse coordinates, and move player towards mouse
     var pointer = this.input.activePointer;
-    this.physics.moveToObject(this.player, pointer, 240);
+    this.physics.moveToObject(this.player, pointer, 600);
 }
 
 
 function add_circular_collider(image)
 {
-    radius = image.displayHeight / 2  // compute radius of circle as half of image height
-    image.body.setCircle(radius);  // set radius of the player's collider size
-    image.body.collideWorldBounds = true;  // allow player to collide with edge of screen
+    radius = image.displayHeight / 2 // compute radius of circle as half of image height
+    image.body.setCircle(radius); // set radius of the player's collider size
+    image.body.collideWorldBounds = true; // allow player to collide with edge of screen
 }
